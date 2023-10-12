49fa076316d76d820a336a7610c358ae7598d9a3
msrnow <147652198+msrnow@users.noreply.github.com>
2023-10-12 19:29:48 +0300

    Create main-categories-names.tsx as static data

---
+++ data/static/main-categories-names.tsx
@@ -0,0 +1,22 @@
+export mainCategories = [
+  { name: 'egypt' },
+  { name: 'sports' },
+  { name: 'world' },
+  { name: 'local' },
+  { name: 'politics' },
+  { name: 'finance' },
+  { name: 'arts' },
+  { name: 'celebrities' },
+  { name: 'movies-news' },
+  { name: 'music-news' },
+  { name: 'tv-news' },
+  { name: 'theater-news' },
+  { name: 'cinema-news' },
+  { name: 'football-news' },
+  { name: 'economy-news' },
+  { name: 'tech-news' },
+  { name: 'phones-news' },
+  { name: 'energy-news' },
+  { name: 'education-news' },
+  { name: 'higher-edu-news' }
+]
