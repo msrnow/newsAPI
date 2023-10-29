1a59e1411fd4018ce9ea25fdac01a22558d7a58b
msrnow <147652198+msrnow@users.noreply.github.com>
2023-10-29 03:22:33 +0200

    Update main-categories-names.tsx

--- data/static/main-categories-names.tsx
+++ data/static/main-categories-names.tsx
@@ -5,18 +5,5 @@ export const mainCategories = [
   { name: 'local' },
   { name: 'politics' },
   { name: 'finance' },
-  { name: 'arts' },
-  { name: 'celebrities' },
-  { name: 'movies-news' },
-  { name: 'music-news' },
-  { name: 'tv-news' },
-  { name: 'theater-news' },
-  { name: 'cinema-news' },
-  { name: 'football-news' },
-  { name: 'economy-news' },
-  { name: 'tech-news' },
-  { name: 'phones-news' },
-  { name: 'energy-news' },
-  { name: 'education-news' },
-  { name: 'higher-edu-news' }
+  { name: 'arts' }
 ]
