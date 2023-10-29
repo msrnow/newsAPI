083af84d3d3fab9e4a2e6fdd9dbe57810746e907
msrnow <147652198+msrnow@users.noreply.github.com>
2023-10-29 05:12:30 +0200

    Create main-news-categories-names.tsx

---
+++ data/static/main-news-categories-names.tsx
@@ -0,0 +1,9 @@
+export const mainNewsCategories = [
+  { name: 'egypt' },
+  { name: 'sports' },
+  { name: 'world' },
+  { name: 'local' },
+  { name: 'politics' },
+  { name: 'finance' },
+  { name: 'arts' }
+]
