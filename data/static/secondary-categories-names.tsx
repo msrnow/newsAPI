445ba651f6071e8018d027519c9fc02cf78d34d7
msrnow <147652198+msrnow@users.noreply.github.com>
2023-10-29 03:22:09 +0200

    Create secondary-categories-names.tsx

---
+++ data/static/secondary-categories-names.tsx
@@ -0,0 +1,15 @@
+export const secondaryCategories = [
+  { name: 'celebrities' },
+  { name: 'movies-news' },
+  { name: 'music-news' },
+  { name: 'tv-news' },
+  { name: 'theater-news' },
+  { name: 'cinema-news' },
+  { name: 'football-news' },
+  { name: 'economy-news' },
+  { name: 'tech-news' },
+  { name: 'phones-news' },
+  { name: 'energy-news' },
+  { name: 'education-news' },
+  { name: 'higher-edu-news' }
+]
