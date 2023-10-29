7e28fa31eeff045d503f5567fe9b00a36a071af0
msrnow <147652198+msrnow@users.noreply.github.com>
2023-10-29 05:21:30 +0200

    comment it out - route.ts

--- app/api/scrape/trigger-all/route.ts
+++ app/api/scrape/trigger-all/route.ts
@@ -1,40 +1,40 @@
-// import { db } from '@/lib/db';
-import { mainNewsCategories } from '@/data/static/main-news-categories-names';
+// // import { db } from '@/lib/db';
+// import { mainNewsCategories } from '@/data/static/main-news-categories-names';
 
-export const fetchCache = 'force-no-store';
+// export const fetchCache = 'force-no-store';
 
-export async function HEAD(request: Request) {
-  console.time('[trigger-all] HEAD');
+// export async function HEAD(request: Request) {
+//   console.time('[trigger-all] HEAD');
 
-  const categories = mainCategories;
-  // const categories = await db.category.findMany({
-  //   where: {
-  //     NOT: {
-  //       name: 'top-headline'
-  //     },
-  //     AND: {
-  //       id: {
-  //         lt: 22
-  //       }
-  //     }
-  //   },
-  //   select: {
-  //     name: true
-  //   }
-  // });
+//   const categories = mainCategories;
+//   // const categories = await db.category.findMany({
+//   //   where: {
+//   //     NOT: {
+//   //       name: 'top-headline'
+//   //     },
+//   //     AND: {
+//   //       id: {
+//   //         lt: 22
+//   //       }
+//   //     }
+//   //   },
+//   //   select: {
+//   //     name: true
+//   //   }
+//   // });
 
-  const baseUrl = process.env.NODE_ENV === 'production' ? 'https://newsapi-msrnow.vercel.app' : 'http://localhost:3000';
-  const triggerOrigin = process.env.NODE_ENV === 'production' ? 'trigger=prod' : 'trigger=local';
+//   const baseUrl = process.env.NODE_ENV === 'production' ? 'https://newsapi-msrnow.vercel.app' : 'http://localhost:3000';
+//   const triggerOrigin = process.env.NODE_ENV === 'production' ? 'trigger=prod' : 'trigger=local';
 
-  let endpoints = categories.map(
-    (category: { name: any }) => `${baseUrl}/api/scrape/googlenews/${category.name}&${triggerOrigin}`
-  );
-  console.log('🚀 ~ file: route.ts:20 ~ endpoints ~ endpoints:', endpoints);
+//   let endpoints = categories.map(
+//     (category: { name: any }) => `${baseUrl}/api/scrape/googlenews/${category.name}&${triggerOrigin}`
+//   );
+//   console.log('🚀 ~ file: route.ts:20 ~ endpoints ~ endpoints:', endpoints);
 
-  fetch(`${baseUrl}/api/scrape/googlenewstopheadlines?${triggerOrigin}`, { method: 'POST' });
+//   fetch(`${baseUrl}/api/scrape/googlenewstopheadlines?${triggerOrigin}`, { method: 'POST' });
 
-  endpoints.map((e: RequestInfo | URL) => fetch(e, { method: 'POST' }));
+//   endpoints.map((e: RequestInfo | URL) => fetch(e, { method: 'POST' }));
 
-  console.timeEnd('[trigger-all] HEAD');
-  return new Response(JSON.stringify('done'), { status: 200 });
-}
+//   console.timeEnd('[trigger-all] HEAD');
+//   return new Response(JSON.stringify('done'), { status: 200 });
+// }
